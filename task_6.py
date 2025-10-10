f1200bbfdb028ab814eec8ec00371ae119a5d35a
cbb <timurkadikovma@gmail.com>
2025-10-10 18:32:29 +0000

    dz

---
+++ task_6.py
@@ -0,0 +1,18 @@
+user_info = []
+
+age = input('Введите возраст пользователя: ')
+user_info.append(age)
+
+gender = input('Введите пол пользователя: ')
+user_info.append(gender)
+
+name = input('Введите имя пользователя: ')
+user_info.append(name)
+
+city = input('Введите город пользователя: ')
+user_info.append(city)
+
+klass = input('Введите класс, в котором учится пользователь: ')
+user_info.append(klass)
+
+print(user_info)
