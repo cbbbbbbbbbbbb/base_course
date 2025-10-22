36436ee3609cc235d338edae187e169c2aaec217
cbb <timurkadikovma@gmail.com>
2025-10-22 16:51:31 +0000

    a

---
+++ lec_cammands_break_and_continue.py
@@ -0,0 +1,15 @@
+'''
+if <условие прерывания>:
+	
+    break / continue
+'''
+
+for symbol in 'Hello world':
+    if symbol == 'o':
+        break
+    print(symbol)
+
+for symbol in 'Hello World':
+    if symbol == 'o':
+        continue
+    print(symbol)
