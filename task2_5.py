1c84f6c8dccf027acbfdaddd5e179526d5fb62c8
cbb <timurkadikovma@gmail.com>
2025-10-24 14:39:34 +0000

    аа

--- task2_5.py
+++ task2_5.py
@@ -5,11 +5,11 @@ if b == 0:
     print('Ошибка: Нельзя делить на ноль!')
 elif a % b == 0:
     chastnoe = a // b
-    print(f"{a} делится на {b} без остатка.")
+    print(f'{a} делится на {b} без остатка.')
     print(f"Частное: {chastnoe}")
 else:
     chastnoe = a // b
     ostatok = a % b
-    print(f"{a} не делится нацело на {b}.")
-    print(f"Частное: {chastnoe}")
-    print(f"Остаток: {ostatok}")
+    print(f'{a} не делится нацело на {b}.')
+    print(f'Частное: {chastnoe}')
+    print(f'Остаток: {ostatok}')
